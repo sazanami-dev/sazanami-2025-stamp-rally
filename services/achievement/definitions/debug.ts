0d07110c5829f223b514ae31926458f1b3331657
rca <nem@nem.x0.to>
2025-11-06 02:03:25 +0900

    remove log (too long)

--- services/achievement/definitions/debug.ts
+++ services/achievement/definitions/debug.ts
@@ -3,11 +3,9 @@ import { AchievementContext, AchievementStrategy } from "../types";
 export const DebugAchievement: AchievementStrategy = {
   id: "debug_achievement",
   async shouldExecute(context: AchievementContext): Promise<boolean> {
-    console.log("DebugAchievement.shouldExecute called with context:", context);
     return true;
   },
   async execute(context: AchievementContext): Promise<boolean> {
-    console.log("DebugAchievement.execute called with context:", context);
     return true;
   },
 };
