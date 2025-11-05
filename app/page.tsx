e34af1ac734609e6d266f0721bcb76041be64c10
rca <nem@nem.x0.to>
2025-11-06 00:52:39 +0900

    add placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 export default function Home() {
   return (
     <>
+      <div className="flex flex-col items-center justify-center h-full gap-6">
+        <p className="text-2xl">Placeholder</p>
+      </div>
     </>
   );
 }
