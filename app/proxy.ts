3daf69e1f1e37de483b1b3cdda3ffe6159237ef8
rca <_@r0m.me>
2025-11-06 16:37:02 +0900

    Revert "debug"
    This reverts commit 5bd5e67c65569a419d3aadeb7451142d7eb15bea.

--- app/proxy.ts
+++
@@ -1,9 +0,0 @@
-import type { NextRequest } from 'next/server'
-import Logger from './lib/logger'
-
-const logger = new Logger('proxy')
- 
-export function proxy(request: NextRequest) {
-  // logger.debug(`Proxying request: ${request.method} ${request.url}`)
-  return new Response('Proxy endpoint is under construction.', { status: 501 });
-}
