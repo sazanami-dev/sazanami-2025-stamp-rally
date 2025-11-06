e2cef50074ecc993894e903c2b5f102e36509d2b
rca <_@r0m.me>
2025-11-06 15:25:43 +0900

    impl simple api wrapper

---
+++ services/auth/api.ts
@@ -0,0 +1,25 @@
+import { ServerEnvUtil, ServerEnvKey } from '@/lib/serverEnv';
+
+import axios from 'axios';
+
+
+const authApi = () => {
+  const apiClient = axios.create({
+    baseURL: ServerEnvUtil.get(ServerEnvKey.CORE_AUTH_BASE_URL)
+  });
+
+  return {
+    get: async (arg: { path: string, params?: Record<string, any> }) => {
+      const { path, params } = arg;
+      const response = await apiClient.get(path, { params });
+      return response.data;
+    },
+    post: async (arg: { path: string, params?: Record<string, any>, data?: Record<string, any> }) => {
+      const { path, params, data } = arg;
+      const response = await apiClient.post(path, data, { params });
+      return response.data;
+    }
+  }
+};
+
+export default authApi;
