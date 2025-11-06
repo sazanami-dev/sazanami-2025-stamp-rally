39a1deb7115cd273fcf7911e54b4efe1f30ad111
rca <_@r0m.me>
2025-11-06 15:25:13 +0900

    トークンデコードなど

---
+++ services/auth/token.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { TokenClaims, TokenClaimsSchema } from '@/types/schema/tokenClaims';
+
+async function decodeToken(token: string): Promise<TokenClaims> {
+  let claims: TokenClaims;
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, (err, decoded) => {
+      if (err) {
+        return reject(err);
+      }
+      try {
+        claims = TokenClaimsSchema.parse(decoded);
+      } catch (e) {
+        return reject(e);
+      }
+      resolve(claims);
+    });
+  });
+}
