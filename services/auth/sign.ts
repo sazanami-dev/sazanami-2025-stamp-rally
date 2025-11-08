97112f725ddc9c062cd9423cd445ead940cd87a5
rca <nem@nem.x0.to>
2025-11-08 17:27:25 +0900

    remove commented codes

--- services/auth/sign.ts
+++ services/auth/sign.ts
@@ -1,37 +1,3 @@
-// import jwks from 'jwk-to-pem';
-// import jwt from 'jsonwebtoken';
-// import authApi from './api';
-//
-// async function getSigningKey(kid: string): Promise<string> {
-//   const jwksResponse = await authApi().get({ path: '/.well-known/jwks.json' });
-//   const jwk = jwksResponse.keys.find((key: any) => key.kid === kid);
-//   if (!jwk) {
-//     throw new Error(`Signing key not found for kid: ${kid}`);
-//   }
-//   return jwks(jwk);
-// }
-//
-// async function verifyToken(token: string): Promise<any> {
-//   const decodedHeader = jwt.decode(token, { complete: true });
-//   if (!decodedHeader || typeof decodedHeader === 'string') {
-//     throw new Error('Invalid token');
-//   }
-//   const kid = decodedHeader.header.kid;
-//   if (!kid) throw new Error('Token header does not contain kid');
-//   const publicKey = await getSigningKey(kid);
-//   return new Promise((resolve, reject) => {
-//     jwt.verify(token, publicKey, (err, decoded) => {
-//       if (err) {
-//         return reject(err);
-//       }
-//       resolve(decoded);
-//     });
-//   });
-// }
-//
-// export { verifyToken };
-
-// Rewrite with jose
 import { jwtVerify, createRemoteJWKSet } from 'jose';
 import { ServerEnvKey, ServerEnvUtil } from '@/lib/serverEnv';
 
