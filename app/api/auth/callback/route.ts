bfce7037a03a80251ae0506ff4cf0a7b78656a9f
rca <nem@nem.x0.to>
2025-11-08 13:39:30 +0900

    impl test handler

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,1 +1,11 @@
-// Placeholder
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// catch post from coreAuth
+
+export async function POST(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const body = await request.json();
+  const token = body.token;
+  console.log('Received token from coreAuth:', token);
+}
