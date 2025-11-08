934d61680139ce1c7da8a24b02c5a451d215378e
rca <nem@nem.x0.to>
2025-11-08 14:03:06 +0900

    improve error handling

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { setToken } from '@/services/auth/waiting';
+import { setToken, WaitingEntryExpiredError, WaitingEntryNotFoundError } from '@/services/auth/waiting';
 import { verifyToken } from '@/services/auth/sign';
 
 // catch post from coreAuth
@@ -19,7 +19,15 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ status: 'error', message: 'Invalid token' }, { status: 400 });
   }
 
-  await setToken(state, token);
+  await setToken(state, token).catch((error) => {
+    if (error instanceof WaitingEntryNotFoundError) {
+      return NextResponse.json({ status: 'error', message: 'Waiting entry not found' }, { status: 404 });
+    } else if (error instanceof WaitingEntryExpiredError) {
+      return NextResponse.json({ status: 'error', message: 'Waiting entry has expired' }, { status: 410 });
+    } else {
+      return NextResponse.json({ status: 'error', message: 'Internal server error' }, { status: 500 });
+    }
+  });
 
   // coreAuthには200を返す
   return NextResponse.json({ status: 'ok' });
