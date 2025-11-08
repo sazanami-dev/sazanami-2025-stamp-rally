f22118dc5ea4d12d98cd09595e8f28bd058fedc2
rca <nem@nem.x0.to>
2025-11-08 13:59:30 +0900

    waitingIdを発行してstateに持たせるようにした

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { verifyToken } from './services/auth/sign'
+import { createWaiting } from './services/auth/waiting'
 import { ServerEnvKey, ServerEnvUtil } from './lib/serverEnv'
 
 export async function middleware(request: NextRequest) {
@@ -35,9 +36,15 @@ export async function middleware(request: NextRequest) {
     const url = request.nextUrl.clone()
     const coreAuthBaseUrl = new URL(ServerEnvUtil.get(ServerEnvKey.CORE_AUTH_BASE_URL));
 
+    /* WONTFIX: スケーラビリティとかを考慮するならMiddlewareがグローバルやDBに依存することは避けるべきらしい
+     * 規模的にどう考えてもスケーラビリティを考慮する必要がないのでとりあえず
+     */
+    const waiting = await createWaiting();
+
     coreAuthBaseUrl.pathname = '/authenticate';
     coreAuthBaseUrl.searchParams.set('redirectUrl', url.toString());
     coreAuthBaseUrl.searchParams.set('postbackUrl', url.origin + '/api/auth/callback');
+    coreAuthBaseUrl.searchParams.set('state', waiting.id);
     return NextResponse.redirect(coreAuthBaseUrl);
   }
 }
