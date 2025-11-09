70ea828e172123004bfb98ef8e6226888ff54520
rca <nem@nem.x0.to>
2025-11-09 16:38:29 +0900

    エラーコードを扱いごとにまとめて定義するように

---
+++ types/const.ts
@@ -0,0 +1,6 @@
+const PrismaErrorCodesByType = {
+  DataNotFound: ["P2001", "P2025"],
+  DataOperationFailed: ["P2002", "P2003", "P2004", "P2006", "P2007", "P2008", "P2009", "P2010", "P2011", "P2012"]
+};
+
+export { PrismaErrorCodesByType };
