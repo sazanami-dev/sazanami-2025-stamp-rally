d3674395da013fcca964398e35db7a1bd52ff4b9
rca <nem@nem.x0.to>
2025-11-09 15:35:45 +0900

    rewrite with jose

--- services/auth/token.ts
+++ services/auth/token.ts
@@ -1,21 +1,18 @@
 import jwt from 'jsonwebtoken';
 import { TokenClaims, TokenClaimsSchema } from '@/types/schema/tokenClaims';
+import { ServerEnvKey, ServerEnvUtil } from '@/lib/serverEnv';
+import { createRemoteJWKSet, jwtVerify } from 'jose';
 
 async function decodeToken(token: string): Promise<TokenClaims> {
-  let claims: TokenClaims;
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, (err, decoded) => {
-      if (err) {
-        return reject(err);
-      }
-      try {
-        claims = TokenClaimsSchema.parse(decoded);
-      } catch (e) {
-        return reject(e);
-      }
-      resolve(claims);
-    });
-  });
+  const coreAuthBaseUrl = new URL(ServerEnvUtil.get(ServerEnvKey.CORE_AUTH_BASE_URL));
+  const jwksUrl = new URL('/.well-known/jwks.json', coreAuthBaseUrl);
+  const JWKS = createRemoteJWKSet(jwksUrl);
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    return TokenClaimsSchema.parse(payload);
+  } catch (error) {
+    throw new Error('Invalid token');
+  }
 }
 
 export { decodeToken };
