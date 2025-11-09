a2da483b486c1e1e7812f80a289115740b4603f2
rca <nem@nem.x0.to>
2025-11-09 14:27:35 +0900

    認証エラー以外も示せるように

--- components/checkin/error.tsx
+++ components/checkin/error.tsx
@@ -5,19 +5,23 @@ import { TbAlertCircle } from "react-icons/tb";
 // Props
 type CheckinErrorProps = {
   error: string;
+  errorDetail?: string;
 };
 
 export default function CheckinError({ error }: CheckinErrorProps) {
-  
+
   const errorText = error || '何らかのエラーが発生しました'; // TODO: Fix
+  const errorDetail = ''; // TODO: Fix
 
   return (
     <Card className="w-[90%] max-w-[24em] h-[16em]">
       <CardBody className="flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center">
-          <TbAlertCircle className="text-8xl text-red-500 py-6"/>
-          <p className="text-md py-1">認証に失敗しました</p>
-          <p className="text-small text-default-500 py-2">{errorText}</p>
+          <TbAlertCircle className="text-8xl text-red-500 py-6" />
+          <p className="text-md py-1">{errorText}</p>
+          {errorDetail &&
+            <p className="text-small text-default-500 py-2">{errorDetail}</p>
+          }
         </div>
       </CardBody>
     </Card>
