35def7cf545b15578f0929c951cd2f326520d479
rca <nem@nem.x0.to>
2025-11-09 17:11:44 +0900

    まだシステムに存在しないユーザーの場合は作成するように

--- app/api/auth/post/route.ts
+++ app/api/auth/post/route.ts
@@ -1,25 +1,84 @@
 import { NextRequest, NextResponse } from "next/server";
 import { resolveWaiting } from "@/services/auth/waiting";
 import { decodeToken } from "@/services/auth/token";
+import { makeErrorPageUrlHelper } from '@/lib/makeErrorPageUrlHelper';
 import Logger from "@/lib/logger";
+import { TokenClaims } from "@/types/schema/tokenClaims";
+import { createUser, isUserExists } from "@/services/user";
+import authApi from "@/services/auth/api";
 
 const logger = new Logger("api", "auth", "post");
 
 export async function GET(request: NextRequest) {
-  if (request.nextUrl.searchParams.get("postAuth") !== "true") {
+  const state = request.nextUrl.searchParams.get("state")!;
+  const isPostAuth = request.nextUrl.searchParams.get("postAuth") === "true";
+  const waiting = await resolveWaiting(state);
+  let claims: TokenClaims | null = null;
+
+  if (!isPostAuth) {
     return NextResponse.next();
   }
-  if (!request.nextUrl.searchParams.get("state")) {
-    // TODO: postAuthかつstateパラメータがないのは不正なのでエラーに飛ばす
+  if (!state) {
+    const errorPageUrl = makeErrorPageUrlHelper(
+      "MISSING_STATE",
+      "認証の事後処理に失敗しました",
+      "stateパラメータは必須です")
+    return NextResponse.redirect(errorPageUrl);
   }
-
-  const state = request.nextUrl.searchParams.get("state")!;
-  const waiting = await resolveWaiting(state);
 
   logger.info(`Post-auth processing for waiting id: ${waiting.id}`);
   logger.debug(`Resolved waiting: ${JSON.stringify(waiting)}`);
 
-  const claims = await decodeToken(waiting.token!);
+
+  try {
+    claims = await decodeToken(waiting.token!);
+    if (!claims || !claims.uid) {
+      throw new Error(); // 直後に拾うので
+    }
+  } catch (error) {
+    const errorPageUrl = makeErrorPageUrlHelper(
+      "INVALID_TOKEN",
+      "無効なトークンです",
+      "提供されたトークンの検証に失敗しました");
+    logger.error(`Failed to decode token for waiting id: ${waiting.id} - ${(error as Error).message}`);
+    return NextResponse.redirect(errorPageUrl);
+  }
+
+  const api = authApi();
+  const userInfo = await api.get(
+    {
+      path: '/i',
+      headers: {
+        Authorization: `Bearer ${waiting.token!}`,
+      },
+    })
+    .then(res => res.data)
+    .catch(error => {
+      logger.error(`Failed to fetch user info for uid: ${claims!.uid} - ${(error as Error).message}`);
+      const errorPageUrl = makeErrorPageUrlHelper(
+        "USER_INFO_FETCH_FAILED",
+        "ユーザー情報の取得に失敗しました",
+        "認証サーバーからユーザー情報を取得できませんでした");
+
+      return NextResponse.redirect(errorPageUrl);
+    });
+
+  if (!await isUserExists(claims.uid)) {
+    await createUser({
+      id: claims.uid,
+      displayName: userInfo.displayName || "No name",
+      generated: true,
+    }).catch((error) => {
+      logger.error(`Error creating user for uid: ${claims!.uid} - ${(error as Error).message}`);
+      const errorPageUrl = makeErrorPageUrlHelper(
+        "USER_CREATION_FAILED",
+        "ユーザーの生成に失敗しました",
+        "新しいユーザーアカウントの生成中にエラーが発生しました");
+      return NextResponse.redirect(errorPageUrl);
+    });
+
+    logger.success(`Created new user for uid: ${claims.uid}`);
+  }
 
   const redirectUrl = request.nextUrl;
   redirectUrl.searchParams.delete("postAuth");
@@ -35,7 +94,7 @@ export async function GET(request: NextRequest) {
   });
 
   logger.success(`User authenticated successfully, redirecting to: ${redirectUrl.toString()}`);
-  
+
   return response;
 }
 
