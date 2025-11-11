a8cfe7234c64cc79fc77207f65bf1c6379da38ef
rca <nem@nem.x0.to>
2025-11-11 14:29:18 +0900

    mapperの定義を使って表示するように

--- components/home/achievement-carousel/item.tsx
+++ components/home/achievement-carousel/item.tsx
@@ -1,8 +1,18 @@
 import { Card, CardBody, CardFooter } from "@heroui/card";
 import { getAchivementMetadata } from "@/lib/front/mappers";
+import { useMemo } from "react";
 
+type AchievementCarouselItemProps = {
+  achievementId: string;
+  onClick?: (achievementId: string) => void;
+}
 
-export default function AchievementCarouselItem() {
+export default function AchievementCarouselItem(props: AchievementCarouselItemProps) {
+  const { achievementId } = props;
+
+  const metadata = useMemo(() => {
+    return getAchivementMetadata(achievementId);
+  }, [achievementId]);
 
   return <>
     <Card
@@ -11,21 +21,17 @@ export default function AchievementCarouselItem() {
       className="" // TODO
       isPressable
       onPress={() => {
-        alert("Not implemented yet");
+        if (props.onClick) {
+          props.onClick(achievementId);
+        }
       }}>
 
-      <CardBody className="p-4 flex items-center justify-center bg-gradient-to-tr from-cyan-200 to-blue-600 rounded-lg relative">
-        {/* ここにバッジになる画像がはいる */}
-        {/* 仮置きで適当な画像 */}
-        <img
-          src="/achievements/placeholder.png"
-          alt="Achievement Badge"
-          className="w-56 h-56 object-contain"
-        />
+      <CardBody className={`p-4 flex items-center justify-center ${metadata.bgClass}`}>
+        {metadata.icon}
       </CardBody>
       <CardFooter
         className="justify-between py-2 absolute font-bold bottom-0 w-full shadow-small z-10 text-lg">
-        <p className="text-sm text-center w-full">テストタイトル</p>
+        <p className={`text-sm text-center w-full ${metadata.fgClass?.text ?? ""}`}>{metadata.title}</p>
       </CardFooter>
     </Card>
   </>
