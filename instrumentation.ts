ec66b7de4cdebf7c301cb0e7b1b00c111ee79e36
rca <nem@nem.x0.to>
2025-11-11 16:29:41 +0900

    debug

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,11 @@
 import Logger from "@/lib/logger";
 import { prisma } from "@/lib/prisma";
+import { ServerEnvUtil, ServerEnvKey } from "./lib/serverEnv";
 
 async function initializeInstrumentation(logger: Logger) {
   logger.info('System check starting...');
   await testDbConnection(logger);
+  await previewEnvVal(logger);
 }
 
 async function testDbConnection(logger: Logger) {
@@ -15,6 +17,11 @@ async function testDbConnection(logger: Logger) {
   } catch (error) {
     dbLogger.error('Database connection failed: ' + (error as Error).message);
   }
+}
+
+async function previewEnvVal(logger: Logger) {
+  const envLogger = logger.getChild('env');
+  envLogger.debug(`BASE_URL: ${ServerEnvUtil.get(ServerEnvKey.BASE_URL)}`);
 }
 
 const logger = new Logger('check');
