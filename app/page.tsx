9468393edcb68768bea25fcf9e734cd5f2f3159a
rca <nem@nem.x0.to>
2025-11-11 13:35:32 +0900

    impl page wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import { fetchCategories, fetchUserCheckins } from "./actions";
+import CheckinList from "@/components/home/checkin-list/list";
 
 export default function Home() {
+  const [checkins, setCheckins] = useState<Array<any>>([]);
+
+  const [categories, setCategories] = useState<Array<any>>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const appendCheckins = async (page: number = 1) => {
+    if (page <= 1) setCheckins([]);
+    const newCheckin = await fetchUserCheckins(page);
+    setCheckins((prevCheckins) => [...prevCheckins, ...newCheckin.checkins]);
+  };
+
+  useEffect(() => {
+    appendCheckins();
+    fetchCategories().then((cats) => setCategories(cats));
+  }, []);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center h-full gap-6 bg-amber-200">
-        <p className="text-2xl">Placeholder</p>
+        <h1 className="text-3xl font-bold mt-8">チェックイン履歴dbg</h1>
+        <CheckinList
+          checkins={checkins}
+          context={{ categories }}
+          loadMoreCallback={() => {
+            const nextPage = currentPage + 1;
+            setCurrentPage(nextPage);
+            appendCheckins(nextPage);
+          }}
+        />
       </div>
     </>
   );
