bbb78813f550667ed208a3e610cb7e21a3e274d7
rca <nem@nem.x0.to>
2025-11-15 06:09:41 +0900

    add/fix test

--- lib/front/mappers.tsx
+++ lib/front/mappers.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React from "react";
 import { IconType } from "react-icons";
 import { IoFastFoodOutline, IoHammerOutline, IoHelpOutline, IoGolfOutline } from "react-icons/io5";
 
--- services/achievement/__tests__/process-helpers.test.ts
+++ services/achievement/__tests__/process-helpers.test.ts
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { isDebugCheckpoint, shouldSkipAchievement } from "../../index";
+import { isDebugCheckpoint, shouldSkipAchievement } from "../index";
 import type { AchievementContext } from "../../types";
 import { createCheckin } from "./helpers";
 
