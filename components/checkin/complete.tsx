1c99efdc4fdbb98cf3e0a463989cb58237a94aa6
rca <nem@nem.x0.to>
2025-11-15 05:20:09 +0900

    獲得したアチーブメントを表示できるように

--- components/checkin/complete.tsx
+++ components/checkin/complete.tsx
@@ -1,12 +1,26 @@
 "use client";
 import { Card, CardBody } from "@heroui/card";
+import { Divider } from "@heroui/divider";
 import { Button } from "@heroui/button";
 import { TbCheck } from "react-icons/tb";
 import { useRouter } from "next/navigation";
+import AchievementListItem from "./achievement-list-item";
+import { useEffect } from "react";
 
+type CheckinCompleteProps = {
+  achievedIds: string[];
+}
 
-export default function CheckinComplete({ achieved: _achieved }: { achieved: string[] }) {
+export default function CheckinComplete(props: CheckinCompleteProps) {
   const router = useRouter();
+
+  let { achievedIds } = props;
+
+  useEffect(() => {
+    if (!achievedIds) {
+      achievedIds = [];
+    }
+  }, [achievedIds]);
 
   return <>
     <Card className="w-[90%] max-w-[24em]">
@@ -14,6 +28,15 @@ export default function CheckinComplete({ achieved: _achieved }: { achieved: str
         <div className="flex flex-col items-center justify-center w-full">
           <TbCheck className="text-8xl text-green-500 py-6" />
           <p className="text-xl py-1">チェックインしました！</p>
+          {achievedIds.length > 0 &&
+            <div className="flex flex-col items-center justify-center w-full">
+              <Divider className="w-[80%] my-4" />
+              <p className="text-sm pb-4 text-gray-700">{achievedIds.length}件のアチーブメントを獲得しました</p>
+              {achievedIds.map((id) => (
+                <AchievementListItem achievementId={id} />
+              ))}
+            </div>
+          }
           <Button
             radius="full"
             className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg my-4 animate-bounce"
