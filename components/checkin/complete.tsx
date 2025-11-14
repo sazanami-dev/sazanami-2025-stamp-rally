b34cf0c0dca8c1371d8b7b1392474475557facb9
rca <nem@nem.x0.to>
2025-11-15 04:04:28 +0900

    戻るボタンが機能するように

--- components/checkin/complete.tsx
+++ components/checkin/complete.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Card, CardBody } from "@heroui/card";
-import { Divider } from "@heroui/divider";
 import { Button } from "@heroui/button";
 import { TbCheck } from "react-icons/tb";
+import { useRouter } from "next/navigation";
 
 
-export default function CheckinComplete({ achieved }: { achieved: string[] }) {
+export default function CheckinComplete({ achieved: _achieved }: { achieved: string[] }) {
+  const router = useRouter();
 
   return <>
     <Card className="w-[90%] max-w-[24em]">
@@ -16,8 +17,8 @@ export default function CheckinComplete({ achieved }: { achieved: string[] }) {
           <Button
             radius="full"
             className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg my-4 animate-bounce"
-            // onClick={() => document.location.href = '/'}>
-            >
+            onPress={() => router.push("/")}
+          >
             ホームに戻る
           </Button>
         </div>
